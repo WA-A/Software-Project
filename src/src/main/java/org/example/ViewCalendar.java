194d112916328ac9096bf60b2612ab5e6bae15eb
wasan <wasanawwad2020@gmail.com>
2024-03-06 17:02:41 +0200

    View Calender Feature (1.16)

---
+++ src/src/main/java/org/example/ViewCalendar.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ViewCalendar {
+
+    @Given("the user is on the calendar page")
+    public void the_user_is_on_the_calendar_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the page loads")
+    public void the_page_loads() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the calendar should display a clear and organized view")
+    public void the_calendar_should_display_a_clear_and_organized_view() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user looks at the calendar view")
+    public void the_user_looks_at_the_calendar_view() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("upcoming meetings should be prominently displayed Team Metting,{int}-{int}-{int},{int}:00AM")
+    public void upcoming_meetings_should_be_prominently_displayed_team_metting_00am(Integer int1, Integer int2, Integer int3, Integer int4) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("each meeting entry should include the meeting title, date, and time")
+    public void each_meeting_entry_should_include_the_meeting_title_date_and_time() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("upcoming meetings should be prominently displayed Project Metting,{int}-{int}-{int},{int}:30PM")
+    public void upcoming_meetings_should_be_prominently_displayed_project_metting_30pm(Integer int1, Integer int2, Integer int3, Integer int4) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("upcoming meetings should be prominently displayed Client Meeting,{int}-{int}-{int},{int}:00AM")
+    public void upcoming_meetings_should_be_prominently_displayed_client_meeting_00am(Integer int1, Integer int2, Integer int3, Integer int4) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("there are important dates marked")
+    public void there_are_important_dates_marked() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("those dates should be visually distinct from regular events {int}-{int}-{int} ,Team Building Event")
+    public void those_dates_should_be_visually_distinct_from_regular_events_team_building_event(Integer int1, Integer int2, Integer int3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("hovering over an important date should display additional information")
+    public void hovering_over_an_important_date_should_display_additional_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
