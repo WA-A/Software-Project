3e0b0112b0894fe025ab6c828db36f3796ca8aa9
yazanedali <edaliyazan6@gmail.com>
2024-03-17 15:17:43 +0200

    second commit

---
+++ src/src/main/java/org/example/users.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+
+import java.sql.Date;
+
+public class users {
+
+    private int user_id;
+    private String first_N;
+    private String Secend_N;
+    private String Last_N;
+    private String email_c;
+    private String pass;
+    private Date BD;
+
+/////Get
+
+}
+
