fda8918a02e9f9073b3ed9de5322a7e3643b56bc
yazanedali <edaliyazan6@gmail.com>
2024-03-24 15:27:20 +0200

    hi

--- src/src/main/java/org/example/yazan.java
+++ src/src/main/java/org/example/yazan.java
@@ -1,8 +1,10 @@
 package org.example;
 
 public class yazan {
+   static int id=10;
     public static void main(String arg[]){
 
         System.out.println("hi");
+        System.out.println(id);
     }
 }
