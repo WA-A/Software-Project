2e90d7f9f3bf24b1f74479373d12f1db1381b50a
yazanedali <edaliyazan6@gmail.com>
2024-03-24 15:55:57 +0200

    Commit

--- src/src/main/java/org/example/yazan.java
+++ src/src/main/java/org/example/yazan.java
@@ -4,7 +4,7 @@ public class yazan {
    static int id=20;
     public static void main(String arg[]){
 
-        System.out.println("hi");
+        System.out.println("hello");
         System.out.println(id);
     }
 }
