14072293d045cf1ffbf6257cc847f1c99182e808
batool kittaneh <batoolkitt1152002@gmail>
2024-03-24 15:53:55 +0200

    hello

--- src/src/main/java/org/example/batool.java
+++ src/src/main/java/org/example/batool.java
@@ -2,11 +2,16 @@ package org.example;
 
 public class batool {
    private int id;
+   private String name;
    batool() {
        id = 0;
+       name=null;
    }
    public void setId(int id){
        this.id=id;
    }
+    public void setId(String name){
+        this.name=name;
+    }
 
 }
