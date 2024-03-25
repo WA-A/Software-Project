e2d01b76d1a851c2106103793c8324fc6c2abc2f
yazanedali <edaliyazan6@gmail.com>
2024-03-25 22:36:11 +0200

    after delete sonar files

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,9 +0,0 @@
-
-b
-2src/src/main/java/org/example/AdminManagement.java,2\b\2bd47f5fd5c021757be4e7ef8f91028706c7ebe8
-Å
-Q.idea/sonarlint/securityhotspotstore/2/b/2bd47f5fd5c021757be4e7ef8f91028706c7ebe8,7\5\756b31c284aa261cfd9b526b15f39e415ad1819c
-]
--.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
-S
-#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
--- src/src/test/java/Softwear_Project_Acceptance_Test/PlaceManageTest.java
+++ src/src/test/java/Softwear_Project_Acceptance_Test/PlaceManageTest.java
@@ -1,67 +1,64 @@
 package Softwear_Project_Acceptance_Test;
 
+import org.example.Packege;
+import org.example.PackageManager;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import static org.junit.Assert.*;
 
 public class PlaceManageTest {
-    @Given("I am logged in as a {string}")
-    public void i_am_logged_in_as_a(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    private static final PackageManager packageManager = new PackageManager();
+    private String lastOperationResult;
+    @Given("I am logged in as a serviceProvider")
+    public void i_am_logged_in_as_a_service_provider() {
+        // Assuming the login mechanism is handled elsewhere or not relevant for these tests
     }
 
-    @When("I enter the following details:")
-    public void i_enter_the_following_details(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+    @When("I add a new venue with details {string}, {string}, {string}, {string}, {string}, {string}")
+    public void i_add_a_new_venue_with_details(String id, String name, String capacity, String amenities, String pricing, String location) {
+        int packageId = Integer.parseInt(id);
+        int packageCapacity = Integer.parseInt(capacity);
+        int packagePrice = Integer.parseInt(pricing);
+        Packege newPackage = new Packege(packageId, packagePrice, packageCapacity, location, name, amenities, "ServiceProviderName"); // Assuming ServiceProviderName is a placeholder
+        lastOperationResult = packageManager.addPackage(newPackage);
     }
 
-    @Then("{string} with Id {string} should be added to the venue list")
-    public void with_id_should_be_added_to_the_venue_list(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    @Then("Id {string} should be added to the venue list")
+    public void id_should_be_added_to_the_venue_list(String id) {
+        assertNotNull(packageManager.getPackage(Integer.parseInt(id)));  }
 
-    @Given("{string} with Id {string} is listed in the venue list")
-    public void with_id_is_listed_in_the_venue_list(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Given("Id {string} is listed in the venue list")
+    public void id_is_listed_in_the_venue_list(String string) {
+        // This step would ensure that a package with the given ID exists; for simplicity, we'll assume it's already added.
     }
 
     @When("I request to remove the venue with Id {string}")
-    public void i_request_to_remove_the_venue_with_id(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_request_to_remove_the_venue_with_id(String id) {
+        lastOperationResult = packageManager.removePackage(Integer.parseInt(id));
     }
 
-    @Then("{string} with Id {string} should be removed from the venue list")
-    public void with_id_should_be_removed_from_the_venue_list(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("Id {string} should be removed from the venue list")
+    public void id_should_be_removed_from_the_venue_list(String id) {
+        assertNull(packageManager.getPackage(Integer.parseInt(id)));
     }
 
-    @When("I update the details for venue with Id {string} as follows:")
-    public void i_update_the_details_for_venue_with_id_as_follows(String string, io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+    @When("I update the venue {string} with name {string}, capacity {string}, amenities {string}, pricing {string}, location {string}")
+    public void i_update_the_venue_with_name_capacity_amenities_pricing_location(String id, String name, String capacity, String amenities, String pricing, String location) {
+        int packageId = Integer.parseInt(id);
+        int packageCapacity = Integer.parseInt(capacity);
+        int packagePrice = Integer.parseInt(pricing);
+        Packege updatedPackage = new Packege(packageId, packagePrice, packageCapacity, location, name, amenities, "ServiceProviderName"); // ServiceProviderName is a placeholder
+        lastOperationResult = packageManager.updatePackage(packageId, updatedPackage);
     }
 
-    @Then("{string} with Id {string} should have updated details in the venue list")
-    public void with_id_should_have_updated_details_in_the_venue_list(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("Id {string} should have updated details in the venue list")
+    public void id_should_have_updated_details_in_the_venue_list(String id) {
+        Packege pkg = packageManager.getPackage(Integer.parseInt(id));
+        assertNotNull(pkg);
+        // Here you could add more detailed assertions to verify the update was successful
     }
 }
+
+
