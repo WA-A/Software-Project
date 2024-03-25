2ceebdc68f2b465821990e7ed1ce3bf40b4cab67
yazanedali <edaliyazan6@gmail.com>
2024-03-25 23:09:34 +0200

    Workspace

--- src/src/main/java/org/example/yazan.java
+++ src/src/main/java/org/example/yazan.java
@@ -4,7 +4,7 @@ public class yazan {
    static int id=40;
     public static void main(String arg[]){
 
-        System.out.println("hello world");
+        System.out.println("hello world jj");
         System.out.println(id);
     }
 }
