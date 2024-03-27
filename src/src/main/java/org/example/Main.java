e494d753fe0233099389e3af5a60ba81c19c73da
yazanedali <edaliyazan6@gmail.com>
2024-03-28 00:48:55 +0200

    trying

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
@@ -90,7 +92,9 @@ public class Main {
             System.out.println("Wasan awwade" );
 
 
+
+
         }
 
-    }
+   }
 }
